f4cf81b2d481c22fdffc4ba69528c5b2e5d0d831
Raghani Madhu <raghanimadhu@gmail.com>
2023-06-23 22:12:57 +0530

    Completed Sorting

--- src/Table.js
+++ src/Table.js
@@ -4,25 +4,67 @@ import { SnacksContext } from "./contexts/SnacksContext";
 function Table() {
   const { snacks, filters, dispatch } = useContext(SnacksContext);
   // console.log(filters);
-
+  // console.log(filters.sorting.sortBy);
   let sortedSnacks;
 
   if (filters.sorting.sortBy.length > 0) {
-    // if (filters.sorting.sortBy in ["id", "price", "calories"]) {
-    sortedSnacks = filters.sorting.ascending
-      ? [...snacks].sort(
-          (
-            { [filters.sorting.sortBy]: token1 },
-            { [filters.sorting.sortBy]: token2 }
-          ) => token1 - token2
-        )
-      : [...snacks].sort(
-          (
-            { [filters.sorting.sortBy]: token1 },
-            { [filters.sorting.sortBy]: token2 }
-          ) => token2 - token1
-        );
-    // }
+    if (["id", "price", "calories"].includes(filters.sorting.sortBy)) {
+      sortedSnacks = filters.sorting.ascending
+        ? [...snacks].sort(
+            (
+              { [filters.sorting.sortBy]: token1 },
+              { [filters.sorting.sortBy]: token2 }
+            ) => token1 - token2
+          )
+        : [...snacks].sort(
+            (
+              { [filters.sorting.sortBy]: token1 },
+              { [filters.sorting.sortBy]: token2 }
+            ) => token2 - token1
+          );
+    } else if (filters.sorting.sortBy === "product_name") {
+      sortedSnacks = filters.sorting.ascending
+        ? [...snacks].sort(
+            (
+              { [filters.sorting.sortBy]: token1 },
+              { [filters.sorting.sortBy]: token2 }
+            ) => token1.charCodeAt(0) - token2.charCodeAt(0)
+          )
+        : [...snacks].sort(
+            (
+              { [filters.sorting.sortBy]: token1 },
+              { [filters.sorting.sortBy]: token2 }
+            ) => token2.charCodeAt(0) - token1.charCodeAt(0)
+          );
+    } else if (filters.sorting.sortBy === "product_weight") {
+      sortedSnacks = filters.sorting.ascending
+        ? [...snacks].sort(
+            (
+              { [filters.sorting.sortBy]: token1 },
+              { [filters.sorting.sortBy]: token2 }
+            ) => token1.slice(0, -1) - token2.slice(0, -1)
+          )
+        : [...snacks].sort(
+            (
+              { [filters.sorting.sortBy]: token1 },
+              { [filters.sorting.sortBy]: token2 }
+            ) => token2.slice(0, -1) - token1.slice(0, -1)
+          );
+    } else {
+      sortedSnacks = filters.sorting.ascending
+        ? [...snacks].sort(
+            (
+              { [filters.sorting.sortBy]: token1 },
+              { [filters.sorting.sortBy]: token2 }
+            ) => token1[0].charCodeAt(0) - token2[0].charCodeAt(0)
+          )
+        : [...snacks].sort(
+            (
+              { [filters.sorting.sortBy]: token1 },
+              { [filters.sorting.sortBy]: token2 }
+            ) => token2[0].charCodeAt(0) - token1[0].charCodeAt(0)
+          );
+    }
   } else {
     sortedSnacks = snacks;
   }
